8c70c6a3fb4d3ad3aa132e9f3e1c53dbe63d943f
Leeseein <huangha607@naver.com>
2024-12-16 12:08:14 +0900

    update: add comments

--- config/db.config.js
+++ config/db.config.js
@@ -6,9 +6,9 @@ module.exports = {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        connectionLimit: 10,
-        waitForConnections: true,
-        queueLimit: 0
+        connectionLimit: 10, // 최대 10개 연결
+        waitForConnections: true, // 연결 풀이 가득차면 대기
+        queueLimit: 0 // 대기 큐 제한 x
     },
     production: { // 운영 환경
 
