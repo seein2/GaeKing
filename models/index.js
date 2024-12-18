369f9e13b52891da26f5f2708bbe5e59b0cee562
Leeseein <huangha607@naver.com>
2024-12-18 20:35:03 +0900

    Fix: 스키마 변경

--- models/index.js
+++ models/index.js
@@ -2,11 +2,14 @@ const db = require('../config/db');
 
 async function initializeDB() {
     try {
-        // 가족그룹 테이블
+        // 강아지 테이블
         await db.query(`
-            CREATE TABLE IF NOT EXISTS family (
-                id INT PRIMARY KEY AUTO_INCREMENT,
-                family_name VARCHAR(20) NOT NULL,
+            CREATE TABLE IF NOT EXISTS dogs (
+                dog_id INT PRIMARY KEY AUTO_INCREMENT,
+                dog_name VARCHAR(50) NOT NULL,
+                birth_date DATE,
+                breed_type VARCHAR(50),
+                gender ENUM('MALE', 'FEMALE'),
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
         `);
@@ -14,13 +17,23 @@ async function initializeDB() {
         // 유저 테이블
         await db.query(`
             CREATE TABLE IF NOT EXISTS users ( 
-                id VARCHAR(100) PRIMARY KEY NOT NULL UNIQUE,
+                user_id VARCHAR(50) PRIMARY KEY NOT NULL UNIQUE,
                 password VARCHAR(255) NOT NULL,
                 user_name VARCHAR(10) NOT NULL UNIQUE,
-                family_id INT,
                 -- provider ENUM('local', 'kakao') DEFAULT 'local',
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+
+        // 강아지-사용자 관계 테이블
+        await db.query(`
+            CREATE TABLE IF NOT EXISTS dog_user (
+                dog_id INT,
+                user_id VARCHAR(100),
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (family_id) REFERENCES family(id)
+                PRIMARY KEY (dog_id, user_id),
+                FOREIGN KEY (dog_id) REFERENCES dogs(dog_id),
+                FOREIGN KEY (user_id) REFERENCES users(user_id)
             );
         `);
 
@@ -32,7 +45,7 @@ async function initializeDB() {
                 token VARCHAR(255),
                 expires_at TIMESTAMP,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (user_id) REFERENCES users(id)
+                FOREIGN KEY (user_id) REFERENCES users(user_id)
             );
         `);
 
