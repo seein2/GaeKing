48e8b8f14337c43ef320423185760573c6bc572f
Leeseein <huangha607@naver.com>
2024-12-19 03:12:24 +0900

    Fix: 라우터 수정

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { join, login, logout, refresh } = require('../controllers/authController');
+const { join, login, logout, refresh, info } = require('../controllers/authController');
 const {authMiddleware} = require('../middlewares/auth');
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post('/join', join);
 router.post('/login', login);
 router.post('/logout', logout);
 router.post('/refresh', refresh);
+router.get('/info', authMiddleware, info);
 
 module.exports = router;
