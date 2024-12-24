aef45dcb55619c22971dc65c3ca81f4aa54f32c0
Leeseein <huangha607@naver.com>
2024-12-24 13:06:23 +0900

    Feat: 강아지 테이블 crud

--- models/dogModel.js
+++ models/dogModel.js
@@ -2,7 +2,7 @@ const db = require('../config/db');
 
 class Dog {
     static async createDog(dog_name, birth_date, breed_type, gender, profile_image) {
-        const [result] = db.query(
+        const [result] = await db.query(
             `INSERT INTO dogs (dog_name, birth_date, breed_type, gender, profile_image)
             VALUES(?, ?, ?, ?, ?)`,
             [dog_name, birth_date, breed_type, gender, profile_image]
@@ -10,12 +10,34 @@ class Dog {
         return result.insertId;
     }
 
-    static async findById(id){
-        const[result] = db.query(
+    static async findById(id) {
+        const [result] = await db.query(
             `SELECT * FROM dogs WHERE id = ?`,
             [id]
         );
         return result[0];
+    }
+
+    static async updateDog(id, { dog_name, birth_date, breed_type, gender, profile_image }) {
+        const [result] = await db.query(
+            `UPDATE dogs SET dog_name = ?, birth_date = ?, breed_type = ?, gender = ?, profile_image = ?
+            WHERE id = ?`,
+            [dog_name, birth_date, breed_type, gender, profile_image, id]
+        );
+        return result;
+    }
+
+    static async deleteDog(id) {
+        const dog = await this.findById(id);
+        if (!dog) {
+            throw new Error('강아지를 찾을 수 없습니다.');
+        }
+
+        const [result] = await db.query(
+            `DELETE FROM dogs where id = ?`,
+            [id]
+        );
+        return result;
     }
 
 };
