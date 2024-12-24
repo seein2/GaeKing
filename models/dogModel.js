559c8a1b52159a805fb3658211ab85215fe8c905
Leeseein <huangha607@naver.com>
2024-12-24 12:40:10 +0900

    Feat: 강아치 찾기 추가

--- models/dogModel.js
+++ models/dogModel.js
@@ -1,13 +1,21 @@
 const db = require('../config/db');
 
 class Dog {
-    static async createDog(dog_name, birth_date, breed_type, gender) {
+    static async createDog(dog_name, birth_date, breed_type, gender, profile_image) {
         const [result] = db.query(
-            `INSERT INTO dogs (dog_name, birth_date, breed_type, gender)
-            VALUES(?, ?, ?, ?)`,
-            [dog_name, birth_date, breed_type, gender]
+            `INSERT INTO dogs (dog_name, birth_date, breed_type, gender, profile_image)
+            VALUES(?, ?, ?, ?, ?)`,
+            [dog_name, birth_date, breed_type, gender, profile_image]
         );
         return result.insertId;
+    }
+
+    static async findById(id){
+        const[result] = db.query(
+            `SELECT * FROM dogs WHERE id = ?`,
+            [id]
+        );
+        return result[0];
     }
 
 };
