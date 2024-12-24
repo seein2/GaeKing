0fef3e445d816990fc5cc2a2a05f9d7a23f8e03e
Leeseein <huangha607@naver.com>
2024-12-24 13:12:42 +0900

    Feat: dog 라우터 추가

--- routes/dogRoutes.js
+++ routes/dogRoutes.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const { register, info } = require('../controllers/dogController');
 const { authMiddleware } = require('../middlewares/auth');
 const router = express.Router();
+
+router.post('/register', register);
+router.get('/info', info);
 
 module.exports = router;
