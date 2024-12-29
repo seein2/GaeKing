38fd00485884a0e47128d20156884edb5b2b7dc9
kimsy0712 <worldkim0712@naver.com>
2024-12-29 17:49:02 +0900

    Feat: 수정, 삭제  라우터 추가

--- routes/dogRoutes.js
+++ routes/dogRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const { register, info } = require('../controllers/dogController');
+const { register, info, update, remove } = require('../controllers/dogController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 const { uploadProfile } = require('../middlewares/uploadMiddleware')
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post('/',uploadProfile.single('profile_image'), register);
+router.post('/', uploadProfile.single('profile_image'), register);
 router.get('/:id', info);
-// router.put('/:id', update);
-// router.delete('/:id', delete);
+router.put('/:id', uploadProfile.single('profile_image'),  update);
+router.delete('/:id', remove);
 
 module.exports = router;
