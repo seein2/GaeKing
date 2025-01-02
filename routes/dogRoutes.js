f0234914fdad109f440f26e89ccac9a36143bc66
Leeseein <huangha607@naver.com>
2025-01-02 18:11:42 +0900

    Fix: 라우터 설정 롤백

--- routes/dogRoutes.js
+++ routes/dogRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register,list, info, update, remove, getProfileDetail } = require('../controllers/dogController');
+const { register,list, info, update, remove } = require('../controllers/dogController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 const { uploadProfile } = require('../middlewares/uploadMiddleware')
 const router = express.Router();
@@ -8,8 +8,7 @@ router.use(authMiddleware);
 
 router.post('/', uploadProfile.single('profile_image'), register);
 router.get('/', list);
-router.get('/:id', getProfileDetail);
-router.get('/:id/edit', info);
+router.get('/:id', info);
 router.put('/:id', uploadProfile.single('profile_image'),  update);
 router.delete('/:id', remove);
 
