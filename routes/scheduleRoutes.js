6bb3f74c1e2c29e6269f3744e358eb3e49834416
kimsy0712 <worldkim0712@naver.com>
2025-01-04 19:15:21 +0900

    Feat: 스케줄 라우터 설정

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { authMiddleware } = require('../middlewares/authMiddleware');
+const {create, completedButton, getByDogAndDate} = require('../controllers/scheduleController');
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post('/');
+router.post('/', create);
+router.patch('/:schedule_id', completedButton);
+router.get('/:dog_id/:date', getByDogAndDate);
 
 module.exports = router;
