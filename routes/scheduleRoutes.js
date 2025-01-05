c0be6cb828b16c3f93b8bd1b98e7a8e24d83c28b
kimsy0712 <worldkim0712@naver.com>
2025-01-05 16:38:13 +0900

    Fix: 스케줄 라우터 수정

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { authMiddleware } = require('../middlewares/authMiddleware');
-const {create, completedButton, getByDogAndDate} = require('../controllers/scheduleController');
+const { create, getScheduleList, getScheduleDetail, update, delete : deleteSchedule } = require('../controllers/scheduleController');
 const router = express.Router();
 
 router.use(authMiddleware);
 
 router.post('/', create);
-router.patch('/:schedule_id', completedButton);
-router.get('/:dog_id/:date', getByDogAndDate);
+router.put('/:schedule_id', update);
+router.delete('/:schedule_id', deleteSchedule);
+router.get('/list/:date', getScheduleList);
+router.get('/detail/:schedule_id', getScheduleDetail);
 
 module.exports = router;
