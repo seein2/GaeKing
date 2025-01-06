7113446f34bcdc6d2bd17de54805562f49df96e6
Leeseein <huangha607@naver.com>
2025-01-06 21:41:41 +0900

    Style: 코드 포맷팅

--- routes/dogRoutes.js
+++ routes/dogRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register,list, info, update, remove,invitation, join_invitation } = require('../controllers/dogController');
+const { register, list, info, update, remove, invitation, join_invitation } = require('../controllers/dogController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 const { uploadProfile } = require('../middlewares/uploadMiddleware');
 const router = express.Router();
@@ -9,7 +9,7 @@ router.use(authMiddleware);
 router.post('/', uploadProfile.single('profile_image'), register);
 router.get('/', list);
 router.get('/:id', info);
-router.put('/:id', uploadProfile.single('profile_image'),  update);
+router.put('/:id', uploadProfile.single('profile_image'), update);
 router.delete('/:id', remove);
 
 //해당 강아지의 멤버 초대
