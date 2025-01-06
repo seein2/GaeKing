6246cc7fea1c569e7b32df0eab6c2cbda33406df
kimsy0712 <worldkim0712@naver.com>
2025-01-06 20:06:24 +0900

    Feat: 초대 라우터 추가

--- routes/dogRoutes.js
+++ routes/dogRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { register,list, info, update, remove } = require('../controllers/dogController');
+const { register,list, info, update, remove,invitation, join_invitation } = require('../controllers/dogController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
-const { uploadProfile } = require('../middlewares/uploadMiddleware')
+const { uploadProfile } = require('../middlewares/uploadMiddleware');
 const router = express.Router();
 
 router.use(authMiddleware);
@@ -11,5 +11,9 @@ router.get('/', list);
 router.get('/:id', info);
 router.put('/:id', uploadProfile.single('profile_image'),  update);
 router.delete('/:id', remove);
+
+//해당 강아지의 멤버 초대
+router.post('/:id/invite', invitation);
+router.post('/join', join_invitation);
 
 module.exports = router;
