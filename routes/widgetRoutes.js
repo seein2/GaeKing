a4d4ce52604d53b97ba9897e8a510027f21bbe91
Leeseein <huangha607@naver.com>
2025-01-12 20:21:43 +0900

    Fix: 위젯 활성화 엔드포인트 post로 변경

--- routes/widgetRoutes.js
+++ routes/widgetRoutes.js
@@ -6,10 +6,10 @@ const router = express.Router();
 router.use(authMiddleware);
 
 // 위젯 설정 상태 조회
-router.get('/settings/:dogId', getSettings);
+router.get('/settings/:dog_id', getSettings);
 // 활성화된 위젯 정보 조회
-router.get('/active/:dogId', getActiveWidgets);
+router.get('/active/:dog_id', getActiveWidgets);
 // 위젯 활성화/비활성화 업데이트
-router.put('/:dogId', updateWidget);
+router.post('/:dog_id', updateWidget);
 
 module.exports = router;
