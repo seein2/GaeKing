e691c4f1ae25f9ca735a8a993d064843d79bf768
Leeseein <huangha607@naver.com>
2025-01-14 17:00:29 +0900

    Feat: 스케쥴 업데이트 엔드포인트 추가

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { create, getScheduleList, getScheduleDetail, updateSchedule, removeSchedule } = require('../controllers/scheduleController');
+const { create, getScheduleList, getScheduleDetail, updateSchedule, removeSchedule, completionSchedule } = require('../controllers/scheduleController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
@@ -10,5 +10,8 @@ router.put('/:schedule_id', updateSchedule);
 router.delete('/:schedule_id', removeSchedule);
 router.get('/list/:date', getScheduleList);
 router.get('/:schedule_id', getScheduleDetail);
+
+// 스케줄 상태 업데이트. (부분 수정은 patch 사용)
+router.patch('/:schedule_id/:instance_id', completionSchedule);
 
 module.exports = router;
