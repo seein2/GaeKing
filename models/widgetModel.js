d9e760fc3e97839cf97570720933f90de2a64670
Leeseein <huangha607@naver.com>
2025-03-01 18:00:02 +0900

    Fix: 생일 위젯 디데이로 반환

--- models/widgetModel.js
+++ models/widgetModel.js
@@ -23,6 +23,38 @@ class Widget {
 
     // 강아지의 활성화된 위젯 정보만 가져오기 
     static async getActiveWidgets(dogId) {
+
+        // 생일을 구하고
+        const [dday] = await db.query(`
+            SELECT DATE_FORMAT(birth_date, '%m-%d') as dday
+            FROM dogs
+            WHERE dog_id = ?
+        `, [dogId]);
+        // 디데이를 구함
+        let dDay = null;
+        if (dday && dday.length > 0) {
+            const birthDateStr = dday[0].dday;
+            if (birthDateStr) {
+                const today = new Date();
+                const currentYear = today.getFullYear();
+
+                // MM-DD 형식의 생일 문자열을 파싱
+                const [birthMonth, birthDay] = birthDateStr.split('-').map(num => parseInt(num));
+
+                // 올해 생일 날짜 생성
+                let birthDate = new Date(currentYear, birthMonth - 1, birthDay);
+
+                // 만약 올해 생일이 이미 지났다면 내년 생일로 계산
+                if (today > birthDate) {
+                    birthDate = new Date(currentYear + 1, birthMonth - 1, birthDay);
+                }
+
+                // 디데이 계산 (밀리초 단위를 일 단위로 변환)
+                const timeDiff = birthDate.getTime() - today.getTime();
+                dDay = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            }
+        }
+
         const [widgets] = await db.query(`
             SELECT 
                 w.widget_type,
@@ -44,12 +76,13 @@ class Widget {
                 w.widget_type,
                 0 as today_count,
                 0 as completed_count,
-                DATE_FORMAT(d.birth_date, '%m-%d') as birth_date
+                ? as birth_date
             FROM widgets w
             JOIN dogs d ON w.dog_id = d.dog_id
             WHERE w.dog_id = ? AND w.is_activated = true
                 AND w.widget_type = '생일'
-        `, [dogId, dogId]);
+        `, [dogId, dDay, dogId]);
+        console.log(widgets);
         return widgets;
         //반환 형태: [{ widget_type: '산책', today_count: 3, completed_count: 1 }, ...]
     };
